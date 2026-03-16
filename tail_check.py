b0bc9ecd319731f7700ab372c91a445e85777571
mnbplus <hachiminamerutoa@gmail.com>
2026-03-16 09:34:29 +0800

    chore: remove stale debug/temp scripts from repo root

--- tail_check.py
+++
@@ -1,5 +0,0 @@
-lines = open('pyaegis/core/taint.py', encoding='utf-8').readlines()
-print(f'Total lines: {len(lines)}')
-print('--- LAST 20 ---')
-for i, l in enumerate(lines[-20:], start=len(lines)-19):
-    print(f'{i:4d}: {l}', end='')
